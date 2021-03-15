bd36f88a04e8376da58d8b844e2cc891e62d7365
Deborah Mendonça <deborah.mendonca@salvus.me>
2021-03-15 16:40:34 -0300

    feat: add homeView

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import include, url  # noqa
 from django.urls import path
 from django.contrib import admin
 from django.shortcuts import redirect
+from common.views import homeView
 
 import django_js_reverse.views
 
 
 urlpatterns = [
+    path("", homeView.as_view(), name="home"),
     path("admin/", admin.site.urls, name="admin"),
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
 ]
