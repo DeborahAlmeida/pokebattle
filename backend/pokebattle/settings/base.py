1f241824ac1912d350e9700ad48ca782fd576c8c
Deborah Mendonça <deborah.mendonca@salvus.me>
2021-03-31 13:45:08 -0300

    Added database conn

--- backend/pokebattle/settings/base.py
+++ backend/pokebattle/settings/base.py
@@ -156,5 +156,5 @@ SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 '''
-db_from_env = dj.dj_database_url.config(conn_max_agr=600)
+db_from_env = dj_database_url.config(conn_max_agr=600)
 DATABASES['default'].update(db_from_env)
