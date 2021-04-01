b4517f958204a3c1bb6ce3dcbc82e1a76ee080f3
Deborah Mendonça <deborah.mendonca@salvus.me>
2021-04-01 20:45:28 -0300

    fix error on prospector 31

--- backend/battle/battles/rounds.py
+++ backend/battle/battles/rounds.py
@@ -1,9 +1,24 @@
+winner = "nobody"
+opponent = 0
+creator = 0
+
 def roundRunning(info):
     pokemon_creator = info[0]
     pokemon_opponent = info[1]
-    winner = "nobody"
-    opponent = 0
-    creator = 0
+   
+    result = runningBattle(pokemon_creator, pokemon_opponent)
+    
+    if (creator > opponent):
+        winner = "creator"
+    elif (creator < opponent):
+        winner = "opponent"
+    else:
+        winner = "creator"
+
+    return winner
+
+
+def runningBattle(pokemon_creator, pokemon_opponent):
     if (pokemon_creator["attack"] > pokemon_opponent["defense"]):
         creator = creator + 1
     if (pokemon_creator["attack"] == pokemon_opponent["defense"]):
@@ -32,11 +47,8 @@ def roundRunning(info):
             creator = creator + 1
             opponent = opponent + 1
 
-    if (creator > opponent):
-        winner = "creator"
-    elif (creator < opponent):
-        winner = "opponent"
-    else:
-        winner = "creator"
-
-    return winner
+    result = {
+        "creator": creator,
+        "opponent": opponent,
+    }
+    return result
