16f8c3dbf2a3fa8602599cb065ca4758d36592c0
Deborah Mendonça <deborah.mendonca@salvus.me>
2021-04-01 15:51:10 -0300

    fix error on prospector 3

--- backend/battle/battles/rounds.py
+++ backend/battle/battles/rounds.py
@@ -1,30 +1,28 @@
+from .models import Battle
+from .views import get_pokemon_from_api
+
+
 def roundRunning(info):
     pokemon_creator = info[0]
     pokemon_opponent = info[1]
     winner = "nobody"
     opponent = 0
     creator = 0
-    if (pokemon_creator["attack"] > pokemon_opponent["defense"] ):
+    if (pokemon_creator["attack"] > pokemon_opponent["defense"]):
        
         creator = creator + 1
-        winnerRoundOne = "creator"
-    if (pokemon_creator["attack"] == pokemon_opponent["defense"] ):
+    if (pokemon_creator["attack"] == pokemon_opponent["defense"]):
         creator = creator + 1
         opponent = opponent + 1
-        winnerRoundOne = "nobody"
     else:
-        winnerRoundOne = "opponent"
         opponent = opponent + 1
 
-    if (pokemon_opponent["attack"] > pokemon_creator["defense"] ):
-            winnerRoundTwo = "opponent"
+    if (pokemon_opponent["attack"] > pokemon_creator["defense"]):
             opponent = opponent + 1
-    if (pokemon_opponent["attack"] == pokemon_creator["defense"] ):
+    if (pokemon_opponent["attack"] == pokemon_creator["defense"]):
         creator = creator + 1
         opponent = opponent + 1
-        winnerRoundTwo = "nobody"
     else:
-        winnerRoundTwo = "creator"
         creator = creator + 1
 
     if (creator > opponent):
@@ -32,15 +30,15 @@ def roundRunning(info):
     elif (creator < opponent):
         winner = "opponent"
     elif (creator == opponent):
-        if (pokemon_opponent["hp"] > pokemon_creator["hp"] ):
+        if (pokemon_opponent["hp"] > pokemon_creator["hp"]):
             winner = "opponent"
             opponent = opponent + 1
-        elif (pokemon_opponent["hp"] < pokemon_creator["hp"] ):
+        elif (pokemon_opponent["hp"] < pokemon_creator["hp"]):
             winner = "creator"
         else: 
             creator = creator + 1
             opponent = opponent + 1
-            winnerRoundTwo = "nobody"
+            winnerRound_two = "nobody"
     
     if (creator > opponent):
         winner = "creator"
@@ -54,27 +52,27 @@ def roundRunning(info):
 
 
 def battleRunning(poke_id, pokemons):
-    battleInfo = Battle.objects.get(id = poke_id)
-    pokeInfo1_creator = get_pokemon_from_api(battleInfo.pk1_creator)
-    pokeInfo2_creator = get_pokemon_from_api(battleInfo.pk2_creator)
-    pokeInfo3_creator = get_pokemon_from_api(battleInfo.pk3_creator)
-    pokeInfoCreatorList = [pokeInfo1_creator, pokeInfo2_creator, pokeInfo3_creator]
-    pokeInfo1_opponent = get_pokemon_from_api(pokemons[0])
-    pokeInfo2_opponent = get_pokemon_from_api(pokemons[1])
-    pokeInfo3_opponent = get_pokemon_from_api(pokemons[2])
+    battle_info = Battle.objects.get(id=poke_id)
+    poke_info1_creator=get_pokemon_from_api(battle_info.pk1_creator)
+    poke_info2_creator=get_pokemon_from_api(battle_info.pk2_creator)
+    poke_info3_creator=get_pokemon_from_api(battle_info.pk3_creator)
+    poke_info_creator_list=[poke_info1_creator, poke_info2_creator, poke_info3_creator]
+    pokeInfo1_opponent=get_pokemon_from_api(pokemons[0])
+    pokeInfo2_opponent=get_pokemon_from_api(pokemons[1])
+    pokeInfo3_opponent=get_pokemon_from_api(pokemons[2])
 
-    roundOne = [pokeInfo1_creator, pokeInfo1_opponent]
-    roundTwo = [pokeInfo2_creator, pokeInfo2_opponent]
-    roundThree = [pokeInfo3_creator, pokeInfo3_opponent]
+    round_one = [poke_info1_creator, pokeInfo1_opponent]
+    round_two = [poke_info2_creator, pokeInfo2_opponent]
+    round_three = [poke_info3_creator, pokeInfo3_opponent]
 
     winner = "nobody"
     opponent = 0
     creator = 0
 
-    battleRounds = [roundOne, roundTwo, roundThree ]
+    battle_rounds = [round_one, round_two, round_three]
 
-    for battleRound in battleRounds: 
-        result = roundRunning(battleRound)
+    for battle_round in battle_rounds: 
+        result = roundRunning(battle_round)
         if result == "creator":
             creator = creator + 1
         else:
