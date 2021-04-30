8fa9023224a5453177172519d8edeede06e634e9
Deborah Mendonça <deborahsoares01@gmail.com>
2021-04-30 16:09:47 -0300

    add new models to battle's app

--- backend/battle/models.py
+++ backend/battle/models.py
@@ -1,20 +1,37 @@
 from django.conf import settings
 from django.db import models
+from pokemon.models import Pokemon
 
 
 class Battle(models.Model):
-    id = models.AutoField(primary_key=True)
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                related_name='+', null=True)
-    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                 related_name='+', null=True)
-    pk1_creator = models.CharField(max_length=200, null=True)
-    pk2_creator = models.CharField(max_length=200, null=True)
-    pk3_creator = models.CharField(max_length=200, null=True)
-    pk1_opponent = models.CharField(max_length=200, null=True)
-    pk2_opponent = models.CharField(max_length=200, null=True)
-    pk3_opponent = models.CharField(max_length=200, null=True)
-    winner = models.CharField(max_length=200, null=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="creator_battles")
+    opponent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="opponent_battles")
+
+    winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="battle_win", null=True)
+    
+    def publish(self):
+        self.save()
+
+
+class Team(models.Model):
+    battle = models.ForeignKey(Battle, on_delete=models.CASCADE, related_name="teams")
+    trainer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="teams")
+    pokemons = models.ManyToManyField(
+        Pokemon, related_name="teams", through="PokemonTeam"
+    )
+
+    def publish(self):
+        self.save()
+
+
+class PokemonTeam(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="teams")
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="+")
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['order']
+        unique_together = [('team', 'pokemon')]
 
     def publish(self):
         self.save()
