add3952504c0b1be059ed4e864ce06161caa87a2
Deborah Mendonça <deborahsoares01@gmail.com>
2021-05-10 13:08:27 -0300

    alter pokemon's model

--- backend/pokemon/models.py
+++ backend/pokemon/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    pokemon_id = models.CharField(max_length=200, null=True)
+    pokemon_id = models.IntegerField(null=True)
     name = models.CharField(max_length=200, null=True)
     attack = models.CharField(max_length=200, null=True)
     defense = models.CharField(max_length=200, null=True)
