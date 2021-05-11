d9235ca175f9bdd38a6580ff5ac792ef0c3a66a6
Deborah Mendonça <deborahsoares01@gmail.com>
2021-05-11 13:39:30 -0300

    add admin

--- backend/battle/admin.py
+++ backend/battle/admin.py
@@ -1,4 +1,6 @@
-# from django.contrib import admin
-# from .models import Battle
+from django.contrib import admin
+from .models import Battle, Team, PokemonTeam
 
-# admin.site.register(Battle)
+admin.site.register(Battle)
+admin.site.register(Team)
+admin.site.register(PokemonTeam)
