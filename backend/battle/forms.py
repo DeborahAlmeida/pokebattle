afcb3bfc0f9c7400564d15b093aceb2e2c98ac79
Deborah Mendonça <deborahsoares01@gmail.com>
2021-05-21 12:58:15 -0300

    update TeamForm to receive many to many values

--- backend/battle/forms.py
+++ backend/battle/forms.py
@@ -3,55 +3,53 @@ from django import forms
 from .models import Battle, PokemonTeam, Team
 from pokemon.models import Pokemon
 
+
+class BattleForm(forms.ModelForm):
+    class Meta:
+        model = Battle
+        fields = ['opponent',]
+
+
 class TeamForm(forms.ModelForm):
     class Meta:
-        model = PokemonTeam
+        model = Team
         fields = [
-        'pokemon_1',
-        'pokemon_2',
-        'pokemon_3'
+            "pokemon_1",
+            "pokemon_2",
+            "pokemon_3",
         ]
 
-    pokemon_1 = forms.IntegerField(
-        label= "Pokemon 1"
+    pokemon_1 = forms.ModelChoiceField(
+        label="Pokemon 1",
+        queryset=Pokemon.objects.all(),
+        required=True,
     )
-    
-    pokemon_2 = forms.IntegerField(
-        label= "Pokemon 2"
+    pokemon_2 = forms.ModelChoiceField(
+        label="Pokemon 2",
+        queryset=Pokemon.objects.all(),
+        required=True,
+    )
+    pokemon_3 = forms.ModelChoiceField(
+        label="Pokemon 3",
+        queryset=Pokemon.objects.all(),
+        required=True,
     )
 
-    pokemon_3 = forms.IntegerField(
-        label= "Pokemon 3"
-    )
-    
-    # def save(self, commit=True):
-    #     PokemonTeam.objects.create(team=Team.objects.get(pk=47), pokemon=Pokemon.objects.get(pokemon_id=18), order=1)
-    #     PokemonTeam.objects.create(team=Team.objects.get(pk=47), pokemon=Pokemon.objects.get(pokemon_id=16), order=2)
-    #     PokemonTeam.objects.create(team=Team.objects.get(pk=47), pokemon=Pokemon.objects.get(pokemon_id=15), order=3)
-    #     instance = super().save(commit=False)
-    #     instance.some_flag = True
-    #     if commit:
-    #         instance.save()
-    #         self.save_m2m()
 
-    #     return instance
-
-
-# class BattleForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Battle
-#         fields = ('opponent')
-
-# class RoundForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Battle
-#         fields = ('creator', 'opponent', 'pk1_creator', 'pk2_creator', 'pk3_creator')
-
-
-# class RoundForm2(forms.ModelForm):
-
-#     class Meta:
-#         model = Battle
-#         fields = ('pk1_opponent', 'pk2_opponent', 'pk3_opponent')
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
+            
+        
+    def save(self, commit=True):
+        data = self.clean()
+        team = Team.objects.get(pk=143)
+        team.pokemons.add(data["pokemon_1"])
+        team.pokemons.add(data["pokemon_2"])
+        team.pokemons.add(data["pokemon_3"])
+        PokemonTeam.objects.filter(team=team, pokemon=data["pokemon_1"]).update(order=1)
+        PokemonTeam.objects.filter(team=team, pokemon=data["pokemon_2"]).update(order=2)
+        PokemonTeam.objects.filter(team=team, pokemon=data["pokemon_3"]).update(order=3)
+        instance = super().save(commit=False)
+        instance.some_flag = True
+        return instance
