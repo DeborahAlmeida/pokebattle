99d6220296c21170be4f1d2a66e5f11fa379c650
Deborah Mendonça <deborahsoares01@gmail.com>
2021-06-03 18:00:15 -0300

    alter table type pokemon

--- backend/pokemon/models.py
+++ backend/pokemon/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Pokemon(models.Model):
     pokemon_id = models.IntegerField(null=True)
     name = models.CharField(max_length=200, null=True)
-    attack = models.CharField(max_length=200, null=True)
-    defense = models.CharField(max_length=200, null=True)
-    hp = models.CharField(max_length=200, null=True)
+    attack = models.IntegerField(null=True)
+    defense = models.IntegerField(null=True)
+    hp = models.IntegerField(null=True)
