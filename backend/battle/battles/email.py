a2387585dd41795accb11f82a252f88313cf5877
Deborah Mendonça <deborahsoares01@gmail.com>
2021-06-14 14:41:15 -0300

    change email credential

--- backend/battle/battles/email.py
+++ backend/battle/battles/email.py
@@ -4,7 +4,7 @@ from templated_email import send_templated_mail
 def result_battle(battle):
     send_templated_mail(
         template_name='results_battle',
-        from_email='deborahmendonca6@gmail.com',
+        from_email='deborah.mendonca@vinta.com.br',
         recipient_list=[battle.creator, battle.opponent],
         context={
             'winner': battle.winner,
