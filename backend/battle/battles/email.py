9801a7c1832466aed990f9f03a069b599f1893ad
Deborah Mendonça <deborahsoares01@gmail.com>
2021-06-17 09:23:09 -0300

    create a email var

--- backend/battle/battles/email.py
+++ backend/battle/battles/email.py
@@ -1,10 +1,11 @@
 from templated_email import send_templated_mail
+from django.conf import settings
 
 
 def result_battle(battle):
     send_templated_mail(
         template_name='results_battle',
-        from_email='deborah.mendonca@vinta.com.br',
+        from_email=settings.EMAIL,
         recipient_list=[battle.creator, battle.opponent],
         context={
             'winner': battle.winner,
