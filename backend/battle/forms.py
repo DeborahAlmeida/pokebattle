51d5b7138b63aa5860f2f9e53044deecf6af3401
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-08 18:53:15 -0300

    fix limit line on forms

--- backend/battle/forms.py
+++ backend/battle/forms.py
@@ -109,12 +109,23 @@ class TeamForm(forms.ModelForm):
         cleaned_data = self.clean()
         instance = super().save()
         # pylint: disable=line-too-long
-        PokemonTeam.objects.create(team=instance,
-                                   pokemon=cleaned_data['pokemon_1_object'], order=cleaned_data['position_pkn_1'])
-        PokemonTeam.objects.create(team=instance,
-                                   pokemon=cleaned_data['pokemon_2_object'], order=cleaned_data['position_pkn_2'])
-        PokemonTeam.objects.create(team=instance,
-                                   pokemon=cleaned_data['pokemon_3_object'], order=cleaned_data['position_pkn_3'])
+        PokemonTeam.objects.create(
+                                   team=instance,
+                                   pokemon=cleaned_data['pokemon_1_object'],
+                                   order=cleaned_data['position_pkn_1']
+                                   )
+
+        PokemonTeam.objects.create(
+                                   team=instance,
+                                   pokemon=cleaned_data['pokemon_2_object'],
+                                   order=cleaned_data['position_pkn_2']
+                                   )
+
+        PokemonTeam.objects.create(
+                                   team=instance,
+                                   pokemon=cleaned_data['pokemon_3_object'],
+                                   order=cleaned_data['position_pkn_3']
+                                   )
         return instance
 
 
