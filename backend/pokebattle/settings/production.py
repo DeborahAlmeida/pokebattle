88ceced67774ee73ffa0e4237402e39302c9e6fb
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-11 22:17:47 -0300

    add var social

--- backend/pokebattle/settings/production.py
+++ backend/pokebattle/settings/production.py
@@ -108,3 +108,5 @@ JS_REVERSE_EXCLUDE_NAMESPACES = ["admin"]
 
 # Sentry
 sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()], release=COMMIT_SHA)
+
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
