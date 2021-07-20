5fb191ba1eab245187fa99a4ebd0b5637f4d1451
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-20 12:01:07 -0300

    fix redefined builtin

--- backend/battle/tests.py
+++ backend/battle/tests.py
@@ -5,10 +5,10 @@ from battle.battles.battle import validate_sum_pokemons
 class PokemonsSumTest(TestCase):
     def test_sum(self):
         pokemons = ['bulbasaur', 'pikachu', 'pidgeot']
-        sum = validate_sum_pokemons(pokemons)
+        sum_pokemons = validate_sum_pokemons(pokemons)
         self.assertTrue(sum)
 
     def test_sum_invalid(self):
         pokemons = ['sawsbuck', 'arcanine', 'goodra']
-        sum = validate_sum_pokemons(pokemons)
+        sum_pokemons = validate_sum_pokemons(pokemons)
         self.assertFalse(sum)
