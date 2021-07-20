b74025d8d51d89c81a7718f261d2289e1df28cbb
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-20 11:52:13 -0300

    create test to sum point of pokemons

--- backend/battle/tests.py
+++ backend/battle/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import TestCase
+from battle.battles.battle import validate_sum_pokemons
 
-# Create your tests here.
+
+class PokemonsSumTest(TestCase):
+    def test_sum(self):
+        pokemons = ['bulbasaur', 'pikachu', 'pidgeot']
+        sum = validate_sum_pokemons(pokemons)
+        self.assertTrue(sum)
+
+    def test_sum_invalid(self):
+        pokemons = ['sawsbuck', 'arcanine', 'goodra']
+        sum = validate_sum_pokemons(pokemons)
+        self.assertFalse(sum)
