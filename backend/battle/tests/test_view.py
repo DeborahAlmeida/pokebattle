cc47e09be2dac04769b91016a8495078a03ca1fd
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-24 06:02:07 -0300

    test team create view and form

--- backend/battle/tests/test_view.py
+++ backend/battle/tests/test_view.py
@@ -1,11 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase, Client
 from model_bakery import baker
 from django.urls import reverse
-from django.core.exceptions import ValidationError
 from battle.models import PokemonTeam, Team
 from battle.battles.battle import get_winner_for
 from users.models import User
-from pokemon.models import Pokemon
+
 
 class GetWinnerTest(TestCase):
     def setUp(self):
@@ -78,73 +78,116 @@ class GetWinnerTest(TestCase):
         with self.assertRaises(IndexError):
             get_winner_for(self.team_creator, self.team_opponent)
 
-'''
-class ListBattlesTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.battle = baker.make('battle.Battle')
-        self.user = User.objects.create(email='deborah.mendonca@vinta.com.br', password='admin')
-        self.user.set_password('admin')
-        self.user.save()
-    
-    def test_login_user_can_acess_battle_list(self):
-        # import ipdb; ipdb.set_trace()
-        self.client.login(username=self.user.email, password='admin')
-        response = self.client.get('/battle/list/')
-        self.assertEqual(response.status_code, 200)
-    
-    def test_data_returns_battle_ids(self):
-        self.client.login(username=self.user.email, password='admin')
-        battles = baker.make('battle.Battle', creator=self.user, _quantity=2)
-        response = self.client.get('/battle/list/')
-        response_qs = response.context_data.get('battle_list')
-        self.assertCountEqual(battles, response_qs)
-'''
-
 
 class AssociatePokemonsToBattleTest(TestCase):
     def setUp(self):
         super().setUp()
         self.client = Client()
-        self.user = User.objects.create(email='example@example.com', password='admin')
+        self.user = User.objects.create(email='example@example.com')
         self.user.set_password('admin')
         self.user.save()
         self.battle = baker.make('battle.Battle', creator=self.user)
-        names = ['pikachu', 'bulbasaur', 'pidgeot', 'test']
+        names = ['pikachu', 'bulbasaur', 'pidgeot', 'wrong_name']
         for pokemon_name in names:
             baker.make("pokemon.Pokemon", name=pokemon_name, attack=60, defense=15, hp=10)
 
-    # def test_user_can_create_team_with_pokemons(self):
-    #     # import ipdb; ipdb.set_trace()
-    #     self.client.login(username=self.user.email, password='admin')
-    #     pokemons_data = {
-    #         "battle": self.battle.id,
-    #         "trainer": self.user.id,
-    #         "pokemon_1": 'pikachu',
-    #         "pokemon_2": 'bulbasaur',
-    #         "pokemon_3": 'pidgeot',
-    #         "position_pkn_1": 1,
-    #         "position_pkn_2": 2,
-    #         "position_pkn_3": 3,
-    #     }
-    #     response = self.client.post(
-    #         reverse("team_create", kwargs={'pk': 1}), pokemons_data, follow=True)
-    #     self.assertEqual(response.status_code, 200)
-    #     team_user = Team.objects.filter(trainer=self.user)
-    #     self.assertTrue(team_user)
-
-    def test_if_returns_error_when_type_wrong_pokemon_name(self):
+    def test_user_can_create_team_with_pokemons(self):
         self.client.login(username=self.user.email, password='admin')
-        import ipdb; ipdb.set_trace()
         pokemons_data = {
             "battle": self.battle.id,
             "trainer": self.user.id,
-            "pokemon_1": 'test',
+            "pokemon_1": 'pikachu',
             "pokemon_2": 'bulbasaur',
             "pokemon_3": 'pidgeot',
             "position_pkn_1": 1,
             "position_pkn_2": 2,
             "position_pkn_3": 3,
         }
-        with self.assertRaisesMessage(ValidationError, 'ERROR: Type the correct pokemons name'):
-            self.client.post(reverse("team_create", kwargs={'pk': 1}), pokemons_data, follow=True)
+        response = self.client.post(
+            reverse("team_create", kwargs={'pk': self.battle.id}), pokemons_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        team_user = Team.objects.filter(trainer=self.user)
+        self.assertTrue(team_user)
+
+    def test_if_returns_error_when_type_wrong_pokemon_name(self):
+        self.client.login(username=self.user.email, password='admin')
+        pokemons_data = {
+            "battle": self.battle.id,
+            "trainer": self.user.id,
+            "pokemon_1": 'wrong_name',
+            "pokemon_2": 'bulbasaur',
+            "pokemon_3": 'pidgeot',
+            "position_pkn_1": 1,
+            "position_pkn_2": 2,
+            "position_pkn_3": 3,
+        }
+        response = self.client.post(reverse("team_create", kwargs={'pk': self.battle.id}), pokemons_data, follow=True)
+        self.assertEqual(response.context_data['form'].errors['__all__'][0], 'ERROR: Type the correct pokemons name')
+
+    def test_if_returns_error_when_missing_position(self):
+        self.client.login(username=self.user.email, password='admin')
+        pokemons_data = {
+            "battle": self.battle.id,
+            "trainer": self.user.id,
+            "pokemon_1": 'pikachu',
+            "pokemon_2": 'bulbasaur',
+            "pokemon_3": 'pidgeot',
+        }
+        with self.assertRaises(KeyError):
+            self.client.post(reverse(
+                "team_create", kwargs={'pk': self.battle.id}), pokemons_data, follow=True)
+
+    def test_if_returns_error_when_user_is_not_permited(self):
+        self.client.login(username=self.user.email, password='admin')
+        user_not_permited = baker.make('users.User')
+        pokemons_data = {
+            "battle": self.battle.id,
+            "trainer": user_not_permited.id,
+            "pokemon_1": 'pikachu',
+            "pokemon_2": 'bulbasaur',
+            "pokemon_3": 'pidgeot',
+            "position_pkn_1": 1,
+            "position_pkn_2": 2,
+            "position_pkn_3": 3,
+        }
+        response = self.client.post(
+            reverse(
+                "team_create",
+                kwargs={'pk': self.battle.id}), pokemons_data, follow=True)
+        self.assertEqual(
+            response.context_data['form'].errors['__all__'][0],
+            'ERROR: You do not have permission for this action.')
+
+    def test_if_returns_error_when_missing_pokemons(self):
+        self.client.login(username=self.user.email, password='admin')
+        pokemons_data = {
+            "battle": self.battle.id,
+            "trainer": self.user.id,
+            "position_pkn_1": 1,
+            "position_pkn_2": 2,
+            "position_pkn_3": 3,
+        }
+        response = self.client.post(
+            reverse(
+                "team_create",
+                kwargs={'pk': self.battle.id}), pokemons_data, follow=True)
+        self.assertEqual(
+            response.context_data['form'].errors['__all__'][0],
+            'ERROR: Select all pokemons')
+
+    def test_if_returns_error_when_battle_does_not_exist(self):
+        self.client.login(username=self.user.email, password='admin')
+        pokemons_data = {
+            "battle": 100,
+            "trainer": self.user.id,
+            "pokemon_1": 'pikachu',
+            "pokemon_2": 'bulbasaur',
+            "pokemon_3": 'pidgeot',
+            "position_pkn_1": 1,
+            "position_pkn_2": 2,
+            "position_pkn_3": 3,
+        }
+        with self.assertRaises(KeyError):
+            self.client.post(reverse(
+                "team_create", kwargs={'pk': 100}), pokemons_data, follow=True)
+
