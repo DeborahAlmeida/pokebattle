f548f3cc57bf3ff4b3735c28e73cd5e0fdc45844
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-26 13:54:38 -0300

    make changes requested

--- backend/battle/tests/test_view.py
+++ backend/battle/tests/test_view.py
@@ -1,119 +1,104 @@
-from django.test import TestCase, Client
+from common.utils.tests import TestCaseUtils
 from model_bakery import baker
 from django.urls import reverse
-from users.models import User
 from battle.models import Battle, PokemonTeam
 from battle.battles.battle import get_winner_for
 
 
-class ListBattlesTest(TestCase):
+class ListBattlesTest(TestCaseUtils):
     def setUp(self):
-        self.client = Client()
-        self.battle = baker.make('battle.Battle')
-        self.user = User.objects.create(email='example@example.com')
-        self.user.set_password('admin')
-        self.user.save()
+        super().setUp()
+        self.battle = baker.make('battle.Battle', creator=self.user)
+        self.battle_from_user = Battle.objects.filter(creator=self.user)
 
     def test_login_user_can_acess_battle_list(self):
-        self.client.login(username=self.user.email, password='admin')
-        response = self.client.get(reverse('battle_list'))
+        response = self.auth_client.get(reverse('battle_list'))
         self.assertEqual(response.status_code, 200)
 
-    def test_data_returns_empty_list(self):
-        self.client.login(username=self.user.email, password='admin')
-        response = self.client.get(reverse('battle_list'))
+    def test_view_returns_empty_list(self):
+        battle = Battle.objects.get(creator=self.user)
+        battle.delete()
+        response = self.auth_client.get(reverse('battle_list'))
         response_qs = response.context_data.get('battle_list')
         self.assertCountEqual(response_qs, [])
 
-    def test_data_returns_one_battle_in_list(self):
-        self.client.login(username=self.user.email, password='admin')
-        battle = baker.make('battle.Battle', creator=self.user)
-        response = self.client.get(reverse('battle_list'))
+    def test_view_returns_one_battle_in_list(self):
+        response = self.auth_client.get(reverse('battle_list'))
         response_qs = response.context_data.get('battle_list')
-        self.assertCountEqual(response_qs, [battle])
+        self.assertCountEqual(response_qs, [self.battle_from_user][0])
 
-    def test_data_returns_few_battle_ids(self):
-        self.client.login(username=self.user.email, password='admin')
-        battles = baker.make('battle.Battle', creator=self.user, _quantity=4)
-        response = self.client.get(reverse('battle_list'))
+    def test_view_returns_few_battle_ids(self):
+        baker.make('battle.Battle', creator=self.user, _quantity=4)
+        response = self.auth_client.get(reverse('battle_list'))
         response_qs = response.context_data.get('battle_list')
-        self.assertCountEqual(battles, response_qs)
+        self.assertCountEqual(self.battle_from_user, response_qs)
 
-    def test_data_returns_a_lot_battle_ids(self):
-        self.client.login(username=self.user.email, password='admin')
-        battles = baker.make('battle.Battle', creator=self.user, _quantity=100)
-        response = self.client.get(reverse('battle_list'))
+    def test_view_returns_a_lot_battle_ids(self):
+        baker.make('battle.Battle', creator=self.user, _quantity=100)
+        response = self.auth_client.get(reverse('battle_list'))
         response_qs = response.context_data.get('battle_list')
-        self.assertCountEqual(battles, response_qs)
+        self.assertCountEqual(self.battle_from_user, response_qs)
 
-    def test_data_an_error_when_the_user_is_not_logged(self):
+    def test_view_an_error_when_the_user_is_not_logged(self):
+        self.auth_client.logout()
         response = self.client.get(reverse('battle_list'))
-        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/battle/list/')
 
-    def test_data_diff_to_verify_it_returns_exactly_len_of_battle_list(self):
-        self.client.login(username=self.user.email, password='admin')
-
+    def test_view_diff_to_verify_it_returns_exactly_len_of_battle_list(self):
         baker.make('battle.Battle', creator=self.user, _quantity=10)
-        battles_initial = Battle.objects.filter(creator=self.user)
-        response_initial = self.client.get(reverse('battle_list'))
+        response_initial = self.auth_client.get(reverse('battle_list'))
         response_qs_initial = response_initial.context_data.get('battle_list')
 
-        self.assertCountEqual(battles_initial, response_qs_initial)
+        self.assertCountEqual(self.battle_from_user, response_qs_initial)
 
         baker.make('battle.Battle', creator=self.user, _quantity=90)
         battles_updated = Battle.objects.filter(creator=self.user)
-        response_updated = self.client.get(reverse('battle_list'))
+        response_updated = self.auth_client.get(reverse('battle_list'))
         response_qs_updated = response_updated.context_data.get('battle_list')
 
         self.assertCountEqual(battles_updated, response_qs_updated)
 
 
-class BattleCreateViewTest(TestCase):
+class BattleCreateViewTest(TestCaseUtils):
     def setUp(self):
         super().setUp()
-        self.client = Client()
-        self.creator = User.objects.create(email='example@example.com')
-        self.creator.set_password('admin')
-        self.creator.save()
         self.opponent = baker.make('users.User')
 
     def test_create_battle_successfully(self):
         battle_data = {
-            "creator": self.creator.id,
+            "creator": self.user.id,
             "opponent": self.opponent.email,
         }
-        self.client.login(username=self.creator.email, password='admin')
-        self.client.post(reverse('battle'), battle_data)
-        battle = Battle.objects.filter(creator=self.creator, opponent=self.opponent)
+        self.auth_client.post(reverse('battle'), battle_data)
+        battle = Battle.objects.filter(creator=self.user, opponent=self.opponent)
         self.assertTrue(battle)
 
     def test_if_returns_error_when_creator_and_opponent_are_the_same_user(self):
         battle_data = {
-            "creator": self.creator.id,
-            "opponent": self.creator.email,
+            "creator": self.user.id,
+            "opponent": self.user.email,
         }
-        self.client.login(username=self.creator.email, password='admin')
-        response = self.client.post(reverse('battle'), battle_data)
+        response = self.auth_client.post(reverse('battle'), battle_data)
         self.assertEqual(
             response.context_data['form'].errors['__all__'][0],
             "ERROR: You can't challenge yourself.")
 
     def test_if_returns_error_when_opponent_doesnt_exist(self):
         battle_data = {
-            "creator": self.creator.id,
-            "opponent": 100,
+            "creator": self.user.id,
         }
-        self.client.login(username=self.creator.email, password='admin')
-        with self.assertRaises(KeyError):
-            self.client.post(reverse('battle'), battle_data)
+        response = self.auth_client.post(reverse('battle'), battle_data)
+        self.assertEqual(
+            response.context_data['form'].errors['__all__'][0],
+            "ERROR: You need to choose an opponent")
 
 
-class GetWinnerTest(TestCase):
+class GetWinnerTest(TestCaseUtils):
     def setUp(self):
         super().setUp()
-        self.creator, self.opponent = baker.make('users.User', _quantity=2)
-        battle = baker.make('battle.Battle', creator=self.creator, opponent=self.opponent)
-        self.team_creator = baker.make('battle.Team', battle=battle, trainer=self.creator)
+        self.opponent = baker.make('users.User')
+        battle = baker.make('battle.Battle', creator=self.user, opponent=self.opponent)
+        self.team_creator = baker.make('battle.Team', battle=battle, trainer=self.user)
         self.team_opponent = baker.make('battle.Team', battle=battle, trainer=self.opponent)
         self.pokemon_1, self.pokemon_2, self.pokemon_3 = baker.make(
             "pokemon.Pokemon",
