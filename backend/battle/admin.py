1db6f4f45f90f83bf822b0913ab8d5c78081ec9e
Deborah Mendonça <deborahsoares01@gmail.com>
2021-07-27 08:23:29 -0300

    test task

--- backend/battle/admin.py
+++ backend/battle/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Battle, Team, PokemonTeam
+from battle.tasks import run_battle_and_send_result_email
 
 
 class PokemonsInline(admin.TabularInline):
@@ -10,6 +11,9 @@ class PokemonsInline(admin.TabularInline):
 class TeamAdmin(admin.ModelAdmin):
     inlines = (PokemonsInline,)
 
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        run_battle_and_send_result_email.delay(obj.battle_id)
 
 admin.site.register(Battle)
 admin.site.register(Team, TeamAdmin)
