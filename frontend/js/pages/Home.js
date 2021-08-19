7686c3523d130a855605d9e481dc74a4952494fe
Deborah Mendonça <deborahsoares01@gmail.com>
2021-08-19 12:45:59 -0300

    improvement of store data in state

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
+
 import { useParams } from 'react-router-dom';
+
 import { apiUrls, getFromApi } from 'utils/api.js';
 
-function Home(props){
-  const [showCreator, Creator] = useState({});
+function Home(){
+  const [battle, setBattle] = useState();
 
   const { id } = useParams();
 
   const getTeamData = async () => {
-    const data = await getFromApi(apiUrls['battleDetail'](id));
-    Creator({
-      creator : data.creator.email,
-      opponent: data.opponent.email,
-      winner: data.winner
-    });
+    const data = await getFromApi(apiUrls.battleDetail(id));
+    setBattle(data);
     return data;
   };
-
+  console.log(battle)
   getTeamData();
-
+  if (!battle){
+    return 'loading'
+  }
   return (
     <div className="battle_container_detail">
       <h1>Battle information</h1>
-      <h2 className="subtitle_detail">Creator: {showCreator.creator ? showCreator.creator : ""}</h2> 
-      <h2 className="subtitle_detail">Opponent: {showCreator.opponent ? showCreator.opponent : ""}</h2>
-      {showCreator.winner ?
+      <h2 className="subtitle_detail">Creator: {battle.creator ? battle.creator.email : ''}</h2> 
+      <h2 className="subtitle_detail">Opponent: {battle.opponent ? battle.opponent.email : ''}</h2>
+      {battle.winner ?
         <>
-        <img class="img_detail" src="https://image.flaticon.com/icons/png/512/2119/2119019.png" alt="trofeu"/>
-        <h2 class="subtitle_detail">The winner is {showCreator.winner}</h2> 
-        </> : "nulo"}
+        <img className="img_detail" src="https://image.flaticon.com/icons/png/512/2119/2119019.png" alt="trofeu"/>
+        <h2 className="subtitle_detail">The winner is {battle.winner ? battle.winner.email : ''}</h2> 
+        </> : 'nulo'}
     </div>
   );
 }
