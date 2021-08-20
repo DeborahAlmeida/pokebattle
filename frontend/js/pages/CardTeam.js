9d24899b055701f1d0049f5be15719aa9aac37ce
Deborah Mendonça <deborahsoares01@gmail.com>
2021-08-20 16:42:43 -0300

    fix part of the eslint errors

--- frontend/js/pages/CardTeam.js
+++ frontend/js/pages/CardTeam.js
@@ -3,12 +3,12 @@ import React from 'react';
 
 import CardPokemon from 'pages/CardPokemon';
 
-function CardTeam(props) {
+function CardTeam({ pokemons }) {
   return (
     <div className="content_card">
-      <CardPokemon pokemon={props.pokemons[0]} />
-      <CardPokemon pokemon={props.pokemons[1]} />
-      <CardPokemon pokemon={props.pokemons[2]} />
+      <CardPokemon pokemon={pokemons[0]} />
+      <CardPokemon pokemon={pokemons[1]} />
+      <CardPokemon pokemon={pokemons[2]} />
     </div>
   );
 }
