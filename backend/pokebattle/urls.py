ee20fd1d2bc0decd21531407a4229211e298c549
Deborah Mendonça <deborahsoares01@gmail.com>
2021-08-20 16:53:02 -0300

    fix pylint error

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('', include('battle.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('battle.urls_api')),
-    path("react/battle/detail/<int:pk>/", TemplateView.as_view(template_name="react/react_template.html"), name="react_template"),
+    path(
+        "react/battle/detail/<int:pk>/",
+        TemplateView.as_view(template_name="react/react_template.html"),
+        name="react_template"),
 ]
