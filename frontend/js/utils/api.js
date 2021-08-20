a4ac4debb5ff3545afba59ac3274788293969018
Deborah Mendonça <deborahsoares01@gmail.com>
2021-08-20 14:35:17 -0300

    fix part of the eslint errors

--- frontend/js/utils/api.js
+++ frontend/js/utils/api.js
@@ -1,24 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 const baseUrl = window.location.host;
 
 const apiUrls = {
-    battleDetail: (id) => (`api/battle/${id}`),
-    currentUser: 'api/user/'
-}
+  battleDetail: (id) => `api/battle/${id}`,
+  currentUser: 'api/user/',
+};
 
 const createTeamUrl = (id) => {
-  const url = `http://${baseUrl}/team/${id}/create/` 
-  return url
-}
+  const url = `http://${baseUrl}/team/${id}/create/`;
+  return url;
+};
 
 const getFromApi = (urlApi) => {
-    const url = `http://${baseUrl}/${urlApi}`
-    const response = axios.get(url)
-      .then(res => {
-        return res.data
-      })
-    return response
-}
+  const url = `http://${baseUrl}/${urlApi}`;
+  const response = axios.get(url).then((res) => {
+    return res.data;
+  });
+  return response;
+};
 
 export { apiUrls, getFromApi, createTeamUrl };
